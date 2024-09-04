91af477b3faef8bf454766623d9bd818b793b3f8
RominaMaita <u202213765@upc.edu.pe>
2024-09-04 16:13:11 -0500

    feat(news): added source and article entities.

---
+++ src/app/news/model/source.entity.ts
@@ -0,0 +1,17 @@
+export class Source {
+  id:string;
+  name:string;
+  url:string;
+  urlToLogo:string;
+
+  constructor() {
+    this.id="";
+    this.name = "";
+    this.url="";
+    this.urlToLogo="";
+  }
+
+
+
+
+}
